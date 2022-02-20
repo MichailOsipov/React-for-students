c94e0b9d256eb08f0d39c91e6311dec927b08dd4
Michail <osimike96@gmail.com>
2022-02-20 23:26:56 +0300

    Init basic react project.

---
+++ src/main-course.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import styles from './main-course.css';
+
+export const MainCourse = () => (
+    <div className={styles.page}>
+        <h1>Hello with styles</h1>
+    </div>
+);
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import { MainCourse } from './main-course';
+
+ReactDOM.render(
+    <MainCourse />,
+    document.getElementById('root')
+);
