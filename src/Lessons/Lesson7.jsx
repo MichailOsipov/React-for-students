63d3f03fb5dc1a2f07eed4039f325bfebeb01db0
Michail <osimike96@gmail.com>
2022-04-01 01:00:06 +0300

    Fix misspeling.

--- src/Lessons/Lesson7.jsx
+++ src/Lessons/Lesson7.jsx
@@ -5,7 +5,7 @@ import {
 } from '../LayoutComponents';
 
 export const Lesson7 = () => (
-    <Section title="Занятие 6. React hooks - альтернатива React-классам">
+    <Section title="Занятие 7. React hooks - альтернатива React-классам">
         <h3>Материалы по хукам:</h3>
         <a href="https://ru.reactjs.org/docs/hooks-intro.html">
             https://ru.reactjs.org/docs/hooks-intro.html
